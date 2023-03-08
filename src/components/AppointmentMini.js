14bfbba6ab1fe049d768b8e38bf8b0dfbbfa0f73
Stephen Pflug <stephen@pflug.us>
2023-03-08 16:20:02 -0600

    AppointmentMini styling

--- src/components/AppointmentMini.js
+++ src/components/AppointmentMini.js
@@ -2,8 +2,13 @@
 
 function AppointmentMini({ appointment }) {
     return(
-        <div className='w-full bg-green-300'>
-            {appointment.title}
+        <div className='flex justify-between w-full bg-green-300 rounded my-1'>
+            <div className='ml-1'>
+                {appointment.title}
+            </div>
+            <div className='mr-1'>
+                {appointment.start}
+            </div>
         </div>
     );
 }
