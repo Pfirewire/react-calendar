8678991cec7bc18eabb1ae57fef26dd10ad9ede8
Stephen Pflug <stephen@pflug.us>
2023-03-08 16:24:04 -0600

    hiding title overflow

--- src/components/AppointmentMini.js
+++ src/components/AppointmentMini.js
@@ -2,8 +2,8 @@
 
 function AppointmentMini({ appointment }) {
     return(
-        <div className='flex justify-between w-full bg-green-300 rounded my-1'>
-            <div className='ml-1'>
+        <div className='flex justify-between w-full bg-green-300 rounded my-1 h-6'>
+            <div className='ml-1 overflow-hidden text-ellipsis'>
                 {appointment.title}
             </div>
             <div className='mr-1'>
