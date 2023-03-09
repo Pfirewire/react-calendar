3700bc7ec4c4b2ac49880662cc10b0272815de9b
Stephen Pflug <stephen@pflug.us>
2023-03-09 07:25:54 -0600

    adding appointments to week view

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -1,7 +1,29 @@
+import {useState} from "react";
+import EditAppointmentModal from "./modals/EditAppointmentModal";
 
 
 function Appointment({ appointment }) {
+    const [showModal, setShowModal] = useState(false);
 
+    const handleOpenModal = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    return(
+        <div className='flex justify-between w-full bg-green-300 rounded my-1 h-6 cursor-pointer' onClick={handleOpenModal}>
+            <div className='ml-1 overflow-hidden text-ellipsis'>
+                {appointment.title}
+            </div>
+            <div className='mr-1'>
+                {appointment.start}
+            </div>
+            {showModal && <EditAppointmentModal appointment={appointment} handleClose={handleCloseModal} />}
+        </div>
+    );
 }
 
 export default Appointment;
--- src/components/pages/WeekPage.js
+++ src/components/pages/WeekPage.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { incrementWeek, decrementWeek } from "../../store";
+import {incrementWeek, decrementWeek, useFetchAppointmentsQuery} from "../../store";
 import Header from "../Header";
 import Week from "../Week";
 
@@ -9,6 +9,7 @@ function WeekPage() {
         const dateAndTime = state.selectedDateAndTime.dateAndTime;
         return new Date(dateAndTime.year, dateAndTime.month, dateAndTime.day);
     });
+    const { data, error, isFetching } = useFetchAppointmentsQuery();
 
     const handlePrevWeek = () => {
         dispatch(decrementWeek());
@@ -18,11 +19,36 @@ function WeekPage() {
         dispatch(incrementWeek());
     };
 
+    const renderAppointments = () => {
+        let appointmentList = [];
+        let date = new Date(new Date(tempDate.getFullYear(), tempDate.getMonth(), tempDate.getDate()).setDate(tempDate.getDate() - tempDate.getDay()));
+        for(let i = 0; i < 7; i++) {
+            appointmentList.push(...(data.filter(appointment => {
+                const appointmentDate = new Date(appointment.date);
+                return date.getFullYear() === appointmentDate.getFullYear() &&
+                    date.getMonth() === appointmentDate.getMonth() &&
+                    date.getDate() === appointmentDate.getDate();
+            })));
+            date = new Date(date.setDate(date.getDate() + 1));
+        }
+        return appointmentList;
+    };
+
+    let appointments;
+    if(isFetching) {
+        appointments = [];
+    } else if(error) {
+        appointments = [];
+    } else {
+        appointments = renderAppointments();
+        console.log(appointments);
+    }
+
     return(
         <div className='flex flex-col'>
             <Header handlePrev={handlePrevWeek} handleNext={handleNextWeek} />
             <div>
-                <Week startDate={tempDate.setDate(tempDate.getDate() - tempDate.getDay())} />
+                <Week appointments={appointments} startDate={tempDate.setDate(tempDate.getDate() - tempDate.getDay())} />
             </div>
         </div>
     );
