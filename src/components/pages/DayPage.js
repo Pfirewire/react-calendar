7ccf4b96a54c06c29b33ac4cc216cc4ffa6df780
Stephen Pflug <stephen@pflug.us>
2023-03-13 19:08:25 -0500

    Updating logic on DayPage component to pass appointment props to Day component

--- src/components/pages/DayPage.js
+++ src/components/pages/DayPage.js
@@ -1,9 +1,13 @@
 import { useDispatch, useSelector } from "react-redux";
-import { incrementDay, decrementDay } from "../../store";
+import {incrementDay, decrementDay, useFetchAppointmentsQuery} from "../../store";
 import Header from "../Header";
 import Day from '../time-periods/Day';
+import app from "../../App";
 
 function DayPage() {
+    const dateAndTime = useSelector(state => state.selectedDateAndTime.dateAndTime);
+    const { data, error, isFetching } = useFetchAppointmentsQuery();
+    const selectedDate = new Date(dateAndTime.year, dateAndTime.month, dateAndTime.day);
     const dispatch = useDispatch();
 
     const handlePrevDay = () => {
@@ -14,11 +18,29 @@ function DayPage() {
         dispatch(incrementDay());
     };
 
+    const renderAppointments = () => {
+        return data.filter(appointment => {
+            const appointmentDate = new Date(appointment.date);
+            return selectedDate.getFullYear() === appointmentDate.getFullYear() &&
+            selectedDate.getMonth() === appointmentDate.getMonth() &&
+            selectedDate.getDate() === appointmentDate.getDate()
+        });
+    };
+
+    let appointments;
+    if(isFetching) {
+        appointments = [];
+    } else if(error) {
+        appointments = [];
+    } else {
+        appointments = renderAppointments();
+    }
+
     return(
         <div className='flex flex-col flex-grow m-6'>
             <Header handlePrev={handlePrevDay} handleNext={handleNextDay} />
             <div className='flex justify-center items-center'>
-                <Day isSingleDay />
+                <Day isSingleDay appointments={appointments} />
             </div>
         </div>
     );
