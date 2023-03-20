baecbf9dc7b67ad8958f7f21623bec74d97f24f8
Stephen Pflug <stephen@pflug.us>
2023-03-20 18:18:06 -0500

    Created TimeBlocks component

---
+++ src/components/time-periods/TimeBlocks.js
@@ -0,0 +1,18 @@
+
+
+function TimeBlocks() {
+    const renderedTimeBlocks = () => {
+        let timeBlocks = [];
+        for(let i = 0; i < 95; i++) {
+
+        }
+    }
+
+    return(
+        <div className='w-40'>
+
+        </div>
+    )
+}
+
+export default TimeBlocks;
